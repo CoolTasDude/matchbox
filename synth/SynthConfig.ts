ab9982e76f1d3382a7d3e695c0501982dbe93093
CoolTasDude <erikmekler@gmail.com>
2026-08-02 11:01:36 -0700

    Max Unison Voices: 9 -> 63

--- synth/SynthConfig.ts
+++ synth/SynthConfig.ts
@@ -1669,7 +1669,7 @@ export class Config {
     public static readonly songDetuneMin: number = 0;
     public static readonly songDetuneMax: number = 500;
     public static readonly unisonVoicesMin: number = 1;
-    public static readonly unisonVoicesMax: number = 9;
+    public static readonly unisonVoicesMax: number = 63; //slarmoo: 9
     public static readonly unisonSpreadMin: number = -96;
     public static readonly unisonSpreadMax: number = 96; 
     public static readonly unisonOffsetMin: number = -96;
